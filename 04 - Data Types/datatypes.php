112ee41bf969852bff84e6283fff1044ac164b32
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-07-27 11:21:38 -0300

    datatypes

---
+++ 04 - Data Types/datatypes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Data types</title>
+</head>
+
+<body>
+
+    <?php
+    $phrase = "Battlemages are the best!";
+    $age = 34;
+    $grade = 23.4453;
+    $isApproved = false;
+    null;
+
+    echo $phrase;
+    ?>
+
+</body>
+
+</html>
