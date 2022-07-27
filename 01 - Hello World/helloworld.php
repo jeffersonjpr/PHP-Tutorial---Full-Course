661e1b4ff9d8ec7b9aa148f3d3e2ffb78e01bd81
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-07-27 10:59:38 -0300

    writting php

--- 01 - Hello World/helloworld.php
+++ 01 - Hello World/helloworld.php
@@ -1,15 +1,18 @@
 <!-- http://localhost:4000/01%20-%20Hello%20World/helloworld.php -->
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Hello</title>
-    </head>
-    <body>
 
-        <?php
-        echo ("Hello World");
-        ?>
-        
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+
+<body>
+
+    <?php
+    echo ("Hello World");
+    ?>
+
+</body>
+
 </html>
---
+++ 02 - Writing HTML/whtml.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Writting HTML</title>
+</head>
+
+<body>
+
+    <?php
+    echo ("<h1> Jeff's site</h1>"); //the code gets converted to html
+    echo ("<hr>");
+    echo ("<p>Site boladão.</p>");
+    ?>
+
+</body>
+
+</html>
