0a9864ba38dac6ad7a84c9448de05b54ab40c8db
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-07-27 11:14:56 -0300

    variables

---
+++ 03 - Variables/variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variables</title>
+</head>
+
+<body>
+
+    <?php
+    $name = "Dalfgan";
+    $age = "541";
+    $goblinskilled = 86;
+
+    echo ("There once was a man named $name.<br>");
+    echo ("He was a $age years old battlemage.<br>");
+    echo ("Once, in a battle, he killed $goblinskilled goblins with a single fireball.<br>");
+
+    $name = "Dalf";
+    echo ("$name really like to clean the middle earth of the Goblins.<br>");
+    ?>
+
+</body>
+
+</html>
